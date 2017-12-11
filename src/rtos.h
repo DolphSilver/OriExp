a9bf7377f616d6e40356e6b1101f2b9e3a88c47e
Per Pettersson <perpettersson@Pers-MacBook-Pro-2.local>
2017-12-11 22:18:42 +0100

    Tredje gangen gillt

---
+++ src/rtos.h
@@ -0,0 +1,17 @@
+/*
+ * rtos.h
+ *
+ *  Created on: 10 dec. 2017
+ *      Author: perpettersson
+ */
+
+#ifndef SRC_RTOS_H_
+#define SRC_RTOS_H_
+
+#define PROCESS_1 function_pointer;
+// used by scheduler initialization function to
+// build task struct, function returns data on the
+// process, function defined in process file.
+#define
+
+#endif /* SRC_RTOS_H_ */
